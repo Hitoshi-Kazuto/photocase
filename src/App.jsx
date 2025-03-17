7ed7eea21cc0a780cc88c7e5ae372d3d1c35bf4f
Shreyas <shreyas.dbg@gmail.com>
2025-03-17 23:36:44 +0530

    Finalizing

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,8 @@ const App = () => {
     bio: "A brief description about yourself. You can write about your interests, skills, or anything you'd like people to know about you.",
     email: "ayushshukla8540@gmail.com",
     location: "Noida, India",
-    phone: "+91 9310063048"
+    phone: "+91 9310063048",
+    vsco: "https://vsco.co/ayushhshuklaa" 
   };
 
   // Add your image paths here
@@ -76,6 +77,15 @@ const App = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
             </svg>
             <span>{personalInfo.phone}</span>
+          </div>
+          {/* New VSCO link */}
+          <div className="flex items-center">
+            <svg className="w-5 h-5 mr-2 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"></path>
+            </svg>
+            <a href={personalInfo.vsco} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+              My VSCO Profile
+            </a>
           </div>
         </div>
       </header>
